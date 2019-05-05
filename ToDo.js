f419b51308a8ccc6f29f817cbc75f62686b68ca8
trapa <1982gaya@gmail.com>
2019-05-05 21:45:44 +0900

    ready to build the app

--- ToDo.js
+++ ToDo.js
@@ -61,7 +61,11 @@ export default class ToDo extends Component {
                                 <Text style={styles.actionText}>✎</Text>
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity onPressOut={() => deleteToDo(id) }>
+                        <TouchableOpacity onPressOut={(event) => {
+                            event.stopPropagation; 
+                            deleteToDo(id); 
+                        }}
+                        >
                             <View style={styles.actionContainer}>
                                 <Text style={styles.actionText}>✗</Text>
                             </View>
@@ -71,7 +75,8 @@ export default class ToDo extends Component {
             </View>
         );
     }
-    _toggleComplete = () => {
+    _toggleComplete = (event) => {
+        event.stopPropagation();
         const { isCompleted, uncompleteToDo, completeToDo, id } = this.props;
         if(isCompleted) {
             uncompleteToDo(id);
@@ -79,12 +84,14 @@ export default class ToDo extends Component {
             completeToDo(id);
         }
     };
-    _startEditing = () => {
+    _startEditing = (event) => {
+        event.stopPropagation();
         this.setState({
             isEditing: true
         });
     };
-    _finishEditing = () => {
+    _finishEditing = (event) => {
+        event.stopPropagation();
         const { toDoValue } = this.state;
         const { id, updateToDo } = this.props;
         updateToDo(id, toDoValue);
